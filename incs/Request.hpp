fc01f798f08b45bc29f7f689f57ad056562baa0b
parallels <ndormoy@student.42.fr>
2022-07-21 16:23:05 +0200

    start request

--- incs/Request.hpp
+++ incs/Request.hpp
@@ -6,9 +6,38 @@
 class Request
 {
 	public:
-		Request(void);
+		Request(void)
+		{
+			_params["Accept-Charset"] = "";
+			_params["Accept-Encoding"] = "";
+			_params["Accept-Language"] = "en";
+			_params["Accept"] ="";
+			_params["Auth-Scheme"] ="";
+			_params["Autorization"] ="";
+			_params["Content-Type"] = "";
+			_params["Content-Length"] = "";
+			_params["Content-Location"] = "";
+			_params["Content-Type"] = "";
+			_params["Content-Disposition"] = "attachment";
+			_params["Connection"] = "keep-alive";
+			_params["Cookie"] ="";
+			_params["Host"] ="";
+			_params["Date"] = "";
+			_params["Last-Modified"] = "";
+			_params["Location"] = "";
+			_params["Retry-After"] = "";
+			_params["Transfer-encoding"] = "";
+			_params["Set-Cookie"] = "";
+			_params["Origin"] = "";
+			_params["Referer"] ="";
+			_params["From"] = "";
+			_method = 0;
+			_path = "";
+		}
 
 		~Request();
+
+		void	parse_request(void);
 	private:
 		std::map<std::string, std::string>	_params;
 		int	_method;
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,30 +1,6 @@
 #include "webserv.hpp"
 
-Request::Request(void)
+void	parse_request()
 {
 	
-	_params["Accept-Charset"] = "";
-	_params["Accept-Encoding"] = "";
-	_params["Accept-Language"] = "en";
-	_params["Accept"] ="";
-	_params["Auth-Scheme"] ="";
-	_params["Autorization"] ="";
-	_params["Content-Type"] = "";
-	_params["Content-Length"] = "";
-	_params["Content-Location"] = "";
-	_params["Content-Type"] = "";
-	_params["Connection"] = "keep-alive";
-	_params["Cookie"] ="";
-	_params["Host"] ="";
-	_params["Date"] = "";
-	_params["Last-Modified"] = "";
-	_params["Location"] = "";
-	_params["Retry-After"] = "";
-	_params["Transfer-encoding"] = "";
-	_params["Set-Cookie"] = "";
-	_params["Origin"] = "";
-	_params["Referer"] ="";
-	_params["From"] = "";
-	_method = 0;
-	_path = "";
 }
