64cea334c46678ff7a703e9b32a9590b34177589
mathias-mrsn <mathias.mrsn@gmail.com>
2022-09-19 18:02:48 +0200

    cgi hello world to fix

---
+++ bin/srcs/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int
+main (void) {
+    std::cout   << "<!DOCTYPE html>\n" 
+                << "<html>\n"
+                << "<head>\n"
+                << "<title>Page Title</title>\n"
+                << "</head>\n"
+                << "<body>\n"
+                << "<h1>Hello World</h1>\n"
+                << "</body>\n"
+                << "</html>\n";
+    return (0);
+}
